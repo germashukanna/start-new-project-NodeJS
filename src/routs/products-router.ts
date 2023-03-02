16dd1d5ee9ec89bf2d6fe572e212041bd3108efe
germashukanna <50459965+germashukanna@users.noreply.github.com>
2023-03-02 15:56:43 +0300

    07 - 1 - input validation, express-validator

--- src/routs/products-router.ts
+++ src/routs/products-router.ts
@@ -1,17 +1,25 @@
 import {Request, Response, Router} from "express";
+import {body, validationResult} from "express-validator";
 import {productsRepository} from "../ropositories/products-repository";
+import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 
 
 export const productsRouter = Router({})
+
+const validationChain = body('title').trim().isLength({min: 3, max: 10}).withMessage('Errors');
 
 productsRouter.get('/', (req: Request, res: Response) => {
     const foundProducts = productsRepository.findProducts(req.query.title?.toString())
     res.send(foundProducts)
 })
-productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = productsRepository.createProducts(req.body.title)
-    res.status(201).send(newProduct)
-})
+
+productsRouter.post('/',
+    validationChain,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const newProduct = productsRepository.createProducts(req.body.title)
+        res.status(201).send(newProduct)
+    })
 productsRouter.get('/:id', (req: Request, res: Response) => {
     let product = productsRepository.getProductById(+req.params.id)
     if (product) {
@@ -21,7 +29,10 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
     }
 
 })
-productsRouter.put('/:id', (req: Request, res: Response) => {
+productsRouter.put('/:id',
+    validationChain,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
     let isUpdated = productsRepository.updateProducts(+req.params.id, req.body.title)
     if (isUpdated) {
         const product = productsRepository.getProductById(+req.params.id)
